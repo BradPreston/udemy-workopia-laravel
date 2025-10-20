587f0243e15ab930ca27051b9e180ecd3bfc420e
Brad Preston <bap5393@gmail.com>
2025-10-19 20:07:40 -0400

    Create a Job factory

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
+    /** @use HasFactory<\Database\Factories\JobFactory> */
+    use HasFactory;
+
     protected $table = 'job_listings';
     protected $fillable = [
         'title',
