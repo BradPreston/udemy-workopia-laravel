e85232b2f1ca0d1c6d7779f055678b33b548db07
Brad Preston <bap5393@gmail.com>
2025-10-23 18:37:45 -0400

    Create file component and use it in Create Job page

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,16 +1,8 @@
 <x-layout>
     <x-slot name="title">Workopia - Create Job</x-slot>
-    <div
-        class="bg-white mx-auto p-8 rounded-lg shadow-md w-full md:max-w-3xl"
-    >
-        <h2 class="text-4xl text-center font-bold mb-4">
-            Create Job Listing
-        </h2>
-        <form
-            method="POST"
-            action="/jobs"
-            enctype="multipart/form-data"
-        >
+    <div class="bg-white mx-auto p-8 rounded-lg shadow-md w-full md:max-w-3xl">
+        <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
+        <form method="POST" action="/jobs" enctype="multipart/form-data">
             @csrf
             {{-- Job Info --}}
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">Job Info</h2>
@@ -66,27 +58,9 @@
 
             <x-inputs.text id="contact_email" name="contact_email" label="Contact Email" type="email" placeholder="Enter contact email" />
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="company_logo"
-                    >Company Logo</label
-                >
-                <input
-                    id="company_logo"
-                    type="file"
-                    name="company_logo"
-                    class="w-full px-4 py-2 border rounded focus:outline-none @error('company_logo') border-red-500 @enderror"
-                />
-                @error('company_logo')
-                    <p class="text-red-500 text-sm mt-1">{{$message}}</p>
-                @enderror
-            </div>
+            <x-inputs.file id="company_logo" name="company_logo" label="Company Logo" />
 
-            <button
-                type="submit"
-                class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none"
-            >
-                Save
-            </button>
+            <button type="submit" class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">Save</button>
         </form>
     </div>
 </x-layout>
