537d5cd5f08df2cd87a39c1bc1bdb2e081999e6c
bradpreston <bap5393@gmail.com>
2025-10-24 20:53:02 -0400

    Add AlpineJS and use it on menu and alert

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 <body class="bg-grey-100">
     <x-header />
@@ -25,7 +26,7 @@
             <x-alert type="error" message="{{ session('error') }}" />
         @endif
         {{-- End display alert messages --}}
-        
+
         {{$slot}}
     </main>
     <script src="{{asset('js/script.js')}}"></script>
