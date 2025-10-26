9cc7e883af548ffe63e0d9ed0f17bae188f3f5a0
Brad Preston <bap5393@gmail.com>
2025-10-26 08:44:02 -0400

    Create login, register, logout functionality

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -10,11 +10,15 @@ $isActive = request()->is('jobs');
         <nav class="hidden md:flex items-center space-x-4">
             <x-nav-link url="/" :active="request()->is('/')">Home</x-nav-link>
             <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>
+            @auth
             <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
+            <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
+            <x-logout-button />
+            <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
+            @else
             <x-nav-link url="/login" :active="request()->is('login')">Login</x-nav-link>
             <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
-            <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
-            <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
+            @endauth
         </nav>
         <button
             id="hamburger"
@@ -32,10 +36,15 @@ $isActive = request()->is('jobs');
         @click.away="open = false"
     >
         <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
+        @auth
         <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
+        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge" :mobile="true">Dashboard</x-nav-link>
+        <x-logout-button />
+        <div class="pt-2"></div>
+        <x-button-link url="/jobs/create" icon="edit" :block="true">Create Job</x-button-link>
+        @else
         <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">Login</x-nav-link>
         <x-nav-link url="/register" :active="request()->is('register')" :mobile="true">Register</x-nav-link>
-        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge" :mobile="true">Dashboard</x-nav-link>
-        <x-button-link url="/jobs/create" icon="edit" :block="true">Create Job</x-button-link>
+        @endauth
     </nav>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,18 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
+// Jobs routes
 Route::resource('/jobs', JobController::class);
+
+// Auth routes
+Route::get('/register', [RegisterController::class, 'register'])->name('register');
+Route::post('/store', [RegisterController::class, 'store'])->name('register.store');
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
