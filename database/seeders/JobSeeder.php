dac5fb921aa70074c298da57272222aee3576911
bradpreston <bap5393@gmail.com>
2025-10-26 19:01:09 -0400

    Add test user to seeder

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -17,12 +17,20 @@ class JobSeeder extends Seeder
         // Load job listings from file
         $jobListings = include database_path('seeders/data/job_listings.php');
 
-        // Get user IDs from user model
-        $userIDs = User::pluck('id')->toArray();
+        // Get test user ID
+        $testUserID = User::where('email', 'test@test.com')->value('id');
 
-        foreach ($jobListings as &$listing) {
-            // Assign user id to listing
-            $listing['user_id'] = $userIDs[array_rand($userIDs)];
+        // Get all other user IDs from user model
+        $userIDs = User::where('email', '!=', 'test@test.com')->pluck('id')->toArray();
+
+        foreach ($jobListings as $index => &$listing) {
+            if ($index < 2) {
+                // Assign the first two listings to the test user
+                $listing['user_id'] = $testUserID;
+            } else {
+                // Assign user id to listing
+                $listing['user_id'] = $userIDs[array_rand($userIDs)];
+            }
 
             // Add timestamps
             $listing['created_at'] = now();
